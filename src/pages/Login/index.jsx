c273bc6a3134ade37b242aa615a2a1bcfda24ec4
Rayan Siqueira <rayan@g1learn.local>
2022-10-07 15:56:35 -0300

    Responsividade da tela de login e criado botão login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,7 @@
 const Input = ({ name, label, ...props }) => (
     <div className="flex flex-col">
-        <label htmlFor={name} className="text-sm text-bold text-gray-500">{label}</label>
-        <input {...props} name={name} id={name} />
+        <label htmlFor={name} className="text-sm text-bold text-gray-500 mb-2">{label}</label>
+        <input {...props} name={name} id={name} className="p-3 border border-gray-700 rounded-xl focus:outline focus:outline-1 focus:outline-gray-700"/>
     </div>
 )
 
@@ -9,16 +9,18 @@ const Input = ({ name, label, ...props }) => (
 export const Login = () => {
     return(
         <div>
-                <header className="container flex justify-center m-w-5xl p-4 border-b border-red-300">
-                    <img src="/imgs/logo-fundo-branco.svg" className="w-32 md:w-40" />
+                <header className="p-4 border-b border-red-300">
+                    <div className="container max-w-xl flex justify-center">
+                        <img src="/imgs/logo-fundo-branco.svg" className="w-32 md:w-40" />
+                    </div>
                 </header>
                 
-                <main className="p-4">
+                <main className="container max-w-xl p-4">
                     <div className="p-4">
                         <h2 className="text-xl font-bold">Entre na sua conta</h2>
                     </div>
 
-                    <form>
+                    <form className="space-y-6">
                         <Input
                             type="text"
                             name="name"
@@ -32,6 +34,10 @@ export const Login = () => {
                             label="Sua senha"
                             placeholder="Digite sua senha"
                         />
+
+                        <button className="w-full text-center text-white bg-red-500 px-6 py-3 rounded-xl">
+                            Criar minha conta
+                        </button>
                     </form>
 
                 </main>
